ade8a1f8bd3e1cb43533bdb43e13327dae6ac7cf
ismaelpq <iperezquinta@gmail.com>
2025-09-24 15:16:51 +0200

    Plantillas HTML Apartado Docencia extensibles

--- docente/urls.py
+++ docente/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.docente, name='homeDocente')
+    path('', views.docente, name='homeDocente'),
+    path('ejercicios/', views.ejercicios, name='ejercicios')
 ]
--- docente/views.py
+++ docente/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def docente(request):
     return render(request, 'docente/homeDocente.html')
+
+def ejercicios(request):
+    return render(request, 'docente/ejercicios.html')
